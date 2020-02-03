e044da08743041b31d3abe47d573dd36addb20c1
Ben Brooks <ben.brooks@couchbase.com>
2020-02-03 12:50:57 +0000

    Switch to use single Ticker in logCollationWorker loop
    This loop is pretty tight, so instanciating a time.After on each iteration is
    very costly for GC. Switch to specify a single time.Ticker and read the tick
    channel instead to avoid generating a large amount of garbage.

--- base/logger.go
+++ base/logger.go
@@ -11,21 +11,24 @@ func FlushLogBuffers() {
 	time.Sleep(loggerCollateFlushDelay)
 }
 
+// logCollationWorker will take log lines over the given channel, and buffer them until either the buffer is full, or the flushTimeout is exceeded.
+// This is to reduce the number of writes to the log files, in order to batch them up as larger collated chunks, whilst maintaining a low-level of latency with the flush timeout.
 func logCollationWorker(collateBuffer chan string, logger *log.Logger, maxBufferSize int, collateFlushTimeout time.Duration) {
-	// This is the temporary buffer we'll store logs in.
+	ticker := time.NewTicker(collateFlushTimeout)
 	logBuffer := make([]string, 0, maxBufferSize)
+
 	for {
 		select {
-		// Add log to buffer and flush to output if it's full.
 		case l := <-collateBuffer:
 			logBuffer = append(logBuffer, l)
+			// flush if the buffer is full
 			if len(logBuffer) >= maxBufferSize {
 				logger.Print(strings.Join(logBuffer, "\n"))
 				// Empty buffer
 				logBuffer = logBuffer[:0]
 			}
-		// Flush the buffer to the output after this time, even if we don't fill it.
-		case <-time.After(collateFlushTimeout):
+		case <-ticker.C:
+			// flush if we have anything sat in the buffer after this time
 			if len(logBuffer) > 0 {
 				logger.Print(strings.Join(logBuffer, "\n"))
 				// Empty buffer
